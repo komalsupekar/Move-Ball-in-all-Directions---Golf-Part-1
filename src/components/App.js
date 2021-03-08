70f563a6cd0a10a19d37f5c6cc36a57ea39e9097
komalsupekar <komalsupekar>
2021-03-08 19:30:54 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,71 @@
-import React, { Component, useState } from "react";
+import React, { Component, useEffect, useState } from "react";
 import '../styles/App.css';
 
-class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            renderBall: false,
-            posi : 0,
-            ballPosition: { left: "0px" }
-        };
-        this.renderChoice = this.renderBallOrButton.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
+const App=()=>{
+    const [renderBall,setRenderBall]=useState({left:0,top:0});
+    const[posi,setPosi]=useState(0);
+    const [ballpos,setBallpos]=useState({left:0,top:0})
+    useEffect(()=>{
+        document.addEventListener("keydown",(event)=>{
+            //all logic will be here
+            console.log("key code is",event.keyCode,ballpos);
+            switch(event.keyCode){
+                case 39:
+                    setPosi({
+                        left:ballpos.left+5,
+                        top:ballpos.top,
+                    });
+                break;
+                case 40:
+                    setPosi({
+                        left:ballpos.left,
+                        top:ballpos.top+5,
+                    });
+                break;
+                case 37:
+                    setPosi({
+                        left:ballpos.left-5,
+                        top:ballpos.top,
+                    });
+                break;
+                case 38:
+                    setPosi({
+                        left:ballpos.left,
+                        top:ballpos.top-5,
+                    });
+                break;
+            }
+        })
+    },[posi]);//add eventlistener during components didmount
+
+    // useEffect(()=>{
+    //     setBallpos(posi);
+    // },[posi])
+    const buttonClickHandler=()=>{
+        setRenderBall(true)
+    };
+    const renderBallOrButton=()=>{
+        if(renderBall)
+        {
+            return <div className="ball" style={{
+                left:ballpos.left +"px",
+                top:ballpos.top +"px,",
+                position:"absolute"}}></div>
+        }
+        else
+        {
+            return <button onClick={buttonClickHandler}>Click for One Ball</button>
+        }
     };
 
-    buttonClickHandler() {
-   
-   }
-    renderBallOrButton() {
-		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
-		} else {
-		    return <button onClick={this.buttonClickHandler} >Click For One Ball</button>
-		}
-    }
+    
+
+
 
     // bind ArrowRight keydown event
-    componentDidMount() {
-      
-    }
-
-    render() {
-        return (
-            <div className="playground">
-                {this.renderBallOrButton()}
-            </div>
-        )
-    }
-}
+        return <div className="playground">{renderBallOrButton()}</div>
+};
 
 
 export default App;
+
