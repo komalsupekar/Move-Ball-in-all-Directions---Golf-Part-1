dc9a12bc7068aef5e4926bd15af1f42e25150c85
komalsupekar <komalsupekar>
2021-03-26 11:48:02 +0530

    updated code

--- src/components/App.js
+++ src/components/App.js
@@ -2,45 +2,51 @@ import React, { Component, useEffect, useState } from "react";
 import '../styles/App.css';
 
 const App=()=>{
-    const [renderBall,setRenderBall]=useState({left:0,top:0});
-    const[posi,setPosi]=useState(0);
+    const [renderBall,setRenderBall]=useState(false);
+    const[posi,setPosi]=useState({left:5,top:5});
     const [ballpos,setBallpos]=useState({left:0,top:0})
-    useEffect(()=>{
-        document.addEventListener("keydown",(event)=>{
+
+    const updateXY=(x,y)=>{
+        console.log(x,y);
+        setBallpos({
+            left:x,
+            top: y,
+            
+        });
+    };
+    // useEffect(()=>{
+        // const keyListener=document.addEventListener("keydown",(event)=>{
             //all logic will be here
-            console.log("key code is",event.keyCode,ballpos);
+            // console.log("key code is",event.keyCode);
+            const handleListner =(event)=>{
             switch(event.keyCode){
                 case 39:
-                    setPosi({
-                        left:ballpos.left+5,
-                        top:ballpos.top,
-                    });
-                break;
-                case 40:
-                    setPosi({
-                        left:ballpos.left,
-                        top:ballpos.top+5,
-                    });
-                break;
-                case 37:
-                    setPosi({
-                        left:ballpos.left-5,
-                        top:ballpos.top,
-                    });
-                break;
+                    updateXY(ballpos.left+5,ballpos.top);
+                    break;
                 case 38:
-                    setPosi({
-                        left:ballpos.left,
-                        top:ballpos.top-5,
-                    });
-                break;
+                    updateXY(ballpos.left,ballpos.top-5);
+                    break;
+                case 37:
+                    updateXY(ballpos.left - 5,ballpos.top);
+                    break;
+                case 40:
+                    updateXY(ballpos.left,ballpos.top + 5);
+                    break;
+                default:
+                    break;
             }
-        })
-    },[posi]);//add eventlistener during components didmount
+        };
+        
+    //add eventlistener during components didmount
 
-    // useEffect(()=>{
-    //     setBallpos(posi);
-    // },[posi])
+    useEffect(()=>{
+        const keyListener=document.addEventListener("keydown",handleListner);
+        console.log("keylistener is",keyListener);
+        return function(){
+            document.removeEventListener("keydown",handleListner);
+        };
+    },[ballpos]);
+
     const buttonClickHandler=()=>{
         setRenderBall(true)
     };
@@ -49,7 +55,7 @@ const App=()=>{
         {
             return <div className="ball" style={{
                 left:ballpos.left +"px",
-                top:ballpos.top +"px,",
+                top:ballpos.top +"px",
                 position:"absolute"}}></div>
         }
         else
